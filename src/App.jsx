66fe2cbdeb867ed04bce20a3f3ead9e205a6b1dc
Aayushi72 <aayushiyadav538@gmail.com>
2023-11-07 23:34:18 -0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import HomePage from "./components/HomePage";
+import VideoPage from "./components/VideoPage";
+import NavBar from "./components/NavBar";
+import LoginPage from "./components/LoginPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/videos" element={<VideoPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Table1.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Table1 = () => {
+  return (
+    <table border="2">
+      <tr>
+        <th>Name</th>
+        <th>Class</th>
+        <th>Roll No.</th>
+        <th>Sec</th>
+      </tr>
+      <tr>
+        <td>Aayushi</td>
+        <td>Btech</td>
+        <td>01</td>
+        <td>D</td>
+      </tr>
+      <tr>
+        <td>Mansi</td>
+        <td>Btech</td>
+        <td>34</td>
+        <td>D</td>
+      </tr>
+    </table>
+  );
+};
+
+export default Table1;
